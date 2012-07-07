2845d7f923cc0571a63ea22e5fa3e3d246ae8d79
Nicholas E. Rabenau <nerab@gmx.net>
2012-07-07 22:33:16 +0200

    Use --version instead of _version, as per TaskWarrior 3rd-Party Application Guidelines

--- lib/twtest/testcase.rb
+++ lib/twtest/testcase.rb
@@ -10,7 +10,7 @@ module TaskWarrior
     module Integration
       class TestCase < ::Test::Unit::TestCase
         def setup
-          assert(task('_version') =~ /2\.\d\.\d/, "The TaskWarrior binary '#{TASK}' must be available and at least v2.0.")
+          assert(task('--version') =~ /2\.\d\.\d/, "The TaskWarrior binary '#{TASK}' must be available and at least v2.0.")
           @data_dir = Dir.mktmpdir
           @taskrc_file = build_taskrc(:data_dir => @data_dir)
         end
--- lib/twtest/version.rb
+++ lib/twtest/version.rb
@@ -1,7 +1,7 @@
 module TaskWarrior
   module Test
     module Integration
-      VERSION = "0.0.4"
+      VERSION = "0.0.5"
     end
   end
 end
